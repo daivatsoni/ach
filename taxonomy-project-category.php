4bd25acba55578e6af12460c42b8debe6acbabd7
Daivat Soni <daivatsoni@gmail.com>
2017-03-17 17:06:10 +0530

    remove navigation code
    wrong navigation code removed.

--- taxonomy-project-category.php
+++ taxonomy-project-category.php
@@ -40,11 +40,6 @@ get_header('category');
 
                 endwhile;
 
-                the_posts_pagination(array(
-                    'prev_text' => twentyseventeen_get_svg(array('icon' => 'arrow-left')) . '<span class="screen-reader-text">' . __('Previous page', 'twentyseventeen') . '</span>',
-                    'next_text' => '<span class="screen-reader-text">' . __('Next page', 'twentyseventeen') . '</span>' . twentyseventeen_get_svg(array('icon' => 'arrow-right')),
-                    'before_page_number' => '<span class="meta-nav screen-reader-text">' . __('Page', 'twentyseventeen') . ' </span>',
-                ));
 
             else :
 
